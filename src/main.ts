e22360a5852a7618cba799a0beeb43ad7ebac5b1
andre2xu <>
2024-05-13 15:00:24 +0100

    The rich text editor class now requires an id of a div element during instantiation.

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,18 @@
+import $ from 'jquery';
+
+
+
 class RichTextEditor {
-    constructor() {
-        
+    TEXT_BOX: JQuery<HTMLDivElement>;
+
+    constructor(textBoxId: string) {
+        const ELEMENT: HTMLElement | null = document.getElementById(textBoxId);
+
+        if (typeof textBoxId !== 'string' || ELEMENT === null || ELEMENT instanceof HTMLDivElement === false) {
+            throw ReferenceError("Must be an id of a div");
+        }
+
+        this.TEXT_BOX = $(ELEMENT);
     };
 };
 
