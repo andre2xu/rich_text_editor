5e3257bf9f2e1aece2ef2fd98d67d6129259bc3d
andre2xu <>
2024-05-14 14:20:04 +0100

    Created a helper for taking out a color element inside of a parent color element.

--- src/helpers/colors.ts
+++ src/helpers/colors.ts
@@ -16,7 +16,83 @@ function getClosestParentColorElement(child: HTMLElement): HTMLElement | undefin
     throw TypeError("Child element must be a color element");
 };
 
+function separateColorElementFromParentColorElement(child: HTMLElement, parent: HTMLElement) {
+    if (isColorElement(child) === false) {
+        throw TypeError("Child element must be a color element");
+    }
+
+    if (isColorElement(parent) === false) {
+        throw TypeError("Parent element must be a color element");
+    }
+
+    const PARENT_RANGE: Range = document.createRange();
+    PARENT_RANGE.selectNode(parent);
+
+    // move parent into a fragment
+    const PARENT_FRAGMENT: DocumentFragment = PARENT_RANGE.extractContents();
+
+    // split parent contents into 3 slices (left, middle, right); the middle slice is the color element to separate
+    const LEFT_SLICE_RANGE: Range = document.createRange();
+    LEFT_SLICE_RANGE.setStartBefore(PARENT_FRAGMENT.children[0]);
+    LEFT_SLICE_RANGE.setEndBefore(child);
+
+    const MIDDLE_SLICE_RANGE: Range = document.createRange();
+    MIDDLE_SLICE_RANGE.selectNodeContents(child);
+
+    const RIGHT_SLICE_RANGE: Range = document.createRange();
+    RIGHT_SLICE_RANGE.setStartAfter(child);
+    RIGHT_SLICE_RANGE.setEndAfter(PARENT_FRAGMENT.children[0]);
+
+    // shallow copy the non-color-element parents of the color element
+    let non_color_element_parents: Node[] = [];
+
+    $(child).parents().each((_: number, p: HTMLElement) => {
+        if (p === parent) {
+            // stop looping if the color element parent has been reached
+            return false;
+        }
+
+        // make a shallow copy of the parent (in order to keep any attributes while leaving the element itself empty)
+        non_color_element_parents.push(p.cloneNode(false));
+    });
+
+    // reverse the array so that the innermost parents start first
+    non_color_element_parents = non_color_element_parents.reverse();
+
+    // extract the slices
+    const LEFT_SLICE_FRAGMENT: DocumentFragment = LEFT_SLICE_RANGE.extractContents();
+    const MIDDLE_SLICE_FRAGMENT: DocumentFragment = MIDDLE_SLICE_RANGE.extractContents();
+    const RIGHT_SLICE_FRAGMENT: DocumentFragment = RIGHT_SLICE_RANGE.extractContents();
+
+    // wrap the contents of the child color element with the non-color-element parents
+    $(non_color_element_parents).each((_: number, element: Node[]) => {
+        if (element instanceof HTMLElement) {
+            const RANGE = document.createRange();
+            RANGE.selectNodeContents(MIDDLE_SLICE_FRAGMENT);
+            RANGE.surroundContents(element);
+        }
+    });
+
+    // create a new fragment that has the color elements separated
+    const NEW_PARENT_FRAGMENT = document.createDocumentFragment();
+
+    NEW_PARENT_FRAGMENT.append(LEFT_SLICE_FRAGMENT);
+    NEW_PARENT_FRAGMENT.append(MIDDLE_SLICE_FRAGMENT);
+    NEW_PARENT_FRAGMENT.append(RIGHT_SLICE_FRAGMENT);
+
+    // delete empty elements
+    $(NEW_PARENT_FRAGMENT).find('*').each((_: number, element: HTMLElement) => {
+        if (element.innerHTML.length === 0) {
+            $(element).remove();
+        }
+    });
+
+    // replace parent fragment with new one
+    PARENT_RANGE.insertNode(NEW_PARENT_FRAGMENT);
+};
+
 export {
     isColorElement,
-    getClosestParentColorElement
+    getClosestParentColorElement,
+    separateColorElementFromParentColorElement
 };
