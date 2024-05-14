6c00287f9d85b32f4cf9e9d5b0adc6a36afd576f
andre2xu <>
2024-05-14 12:28:19 +0100

    Created a file for helpers that handle color elements. Added a helper that gets the closest color element parent of a color element.

---
+++ src/helpers/colors.ts
@@ -0,0 +1,22 @@
+import $ from 'jquery';
+
+
+
+function getClosestParentColorElement(childId: string) {
+    let child: JQuery<HTMLElement> | HTMLElement | null = document.getElementById(childId);
+
+    if (child !== null) {
+        child = $(child);
+
+        child = child.parents('span[data-type="color"]').first()[0];
+
+        return child;
+    }
+    else {
+        throw ReferenceError("There is no element with that id.");
+    }
+};
+
+export {
+    getClosestParentColorElement
+};
