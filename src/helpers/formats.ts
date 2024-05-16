cc3bd4615d72b1dc386ad2a47f6e952732d6bc83
andre2xu <>
2024-05-16 22:47:22 +0100

    Corrected the logic of the 'isFormatElement' format-styling helper.

--- src/helpers/formats.ts
+++ src/helpers/formats.ts
@@ -3,7 +3,7 @@ import $ from 'jquery';
 
 
 function isFormatElement(element: HTMLElement) {
-    return $.inArray(element.tagName.toLowerCase(), ['b', 'i', 'u', 's']);
+    return $.inArray(element.tagName.toLowerCase(), ['b', 'i', 'u', 's']) !== -1;
 };
 
 export {
