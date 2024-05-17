d5f58a2f312e4b6067e49ceec1e47dfe0344bac8
andre2xu <>
2024-05-17 12:00:35 +0100

    Created a constant for the selector of format elements.

--- src/helpers/formats.ts
+++ src/helpers/formats.ts
@@ -2,6 +2,8 @@ import $ from 'jquery';
 
 
 
+const FORMAT_ELEMENT_SELECTOR: string = 'b, i, u, s';
+
 function isFormatElement(element: HTMLElement) {
     return $.inArray(element.tagName.toLowerCase(), ['b', 'i', 'u', 's']) !== -1;
 };
